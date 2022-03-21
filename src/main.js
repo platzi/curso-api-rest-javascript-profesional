dd83dbcef02625b3b6f449fcccebbd70a45021ba
Juan David Castro <me@juandc.co>
2022-03-21 12:30:09 -0500

    Default images

--- src/main.js
+++ src/main.js
@@ -37,6 +37,12 @@ function createMovies(movies, container, lazyLoad = false) {
       lazyLoad ? 'data-img' : 'src',
       'https://image.tmdb.org/t/p/w300' + movie.poster_path,
     );
+    movieImg.addEventListener('error', () => {
+      movieImg.setAttribute(
+        'src',
+        'https://static.platzi.com/static/images/error/img404.png',
+      );
+    })
 
     if (lazyLoad) {
       lazyLoader.observe(movieImg);
@@ -93,7 +99,7 @@ async function getMoviesByCategory(id) {
   });
   const movies = data.results;
 
-  createMovies(movies, genericSection);
+  createMovies(movies, genericSection, true);
 }
 
 async function getMoviesBySearch(query) {
