c60dea083e63ace9442313a8450ec294edcb0c4e
Juan David Castro <me@juandc.co>
2022-03-21 15:34:48 -0500

    Load more btn

--- src/main.js
+++ src/main.js
@@ -20,8 +20,17 @@ const lazyLoader = new IntersectionObserver((entries) => {
   });
 });
 
-function createMovies(movies, container, lazyLoad = false) {
-  container.innerHTML = '';
+function createMovies(
+  movies,
+  container,
+  {
+    lazyLoad = false,
+    clean = true,
+  } = {},
+) {
+  if (clean) {
+    container.innerHTML = '';
+  }
 
   movies.forEach(movie => {
     const movieContainer = document.createElement('div');
@@ -117,7 +126,35 @@ async function getTrendingMovies() {
   const { data } = await api('trending/movie/day');
   const movies = data.results;
 
-  createMovies(movies, genericSection);
+  createMovies(movies, genericSection, { lazyLoad: true, clean: true });
+
+  const btnLoadMore = document.createElement('button');
+  btnLoadMore.innerText = 'Cargar más';
+  btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+  genericSection.appendChild(btnLoadMore);
+}
+
+let page = 1;
+
+async function getPaginatedTrendingMovies() {
+  page++;
+  const { data } = await api('trending/movie/day', {
+    params: {
+      page,
+    },
+  });
+  const movies = data.results;
+
+  createMovies(
+    movies,
+    genericSection,
+    { lazyLoad: true, clean: false },
+  );
+
+  const btnLoadMore = document.createElement('button');
+  btnLoadMore.innerText = 'Cargar más';
+  btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+  genericSection.appendChild(btnLoadMore);
 }
 
 async function getMovieById(id) {
