2812b76fc81a93eac4c5e76d5f3ae8c6af75de2c
Juan David Castro <me@juandc.co>
2022-03-22 11:12:39 -0500

    local storage 1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// Data
+
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
@@ -7,6 +9,32 @@ const api = axios.create({
     'api_key': API_KEY,
   },
 });
+
+function likedMoviesList() {
+  const item = JSON.parse(localStorage.getItem('liked_movies'));
+  let movies;
+
+  if (item) {
+    movies = item;
+  } else {
+    movies = {};
+  }
+  
+  return movies;
+}
+
+function likeMovie(movie) {
+  // movie.id
+  const likedMovies = likedMoviesList();
+
+  if (likedMovies[movie.id]) {
+    likedMovies[movie.id] = undefined;
+  } else {
+    likedMovies[movie.id] = movie;
+  }
+
+  localStorage.setItem('liked_movies', JSON.stringify(likedMovies));
+}
 
 
 // Utils
@@ -57,7 +85,7 @@ function createMovies(
     movieBtn.classList.add('movie-btn');
     movieBtn.addEventListener('click', () => {
       movieBtn.classList.toggle('movie-btn--liked');
-      // DEBERIAMOS AGREGAR LA PELICULA A LS
+      likeMovie(movie);
     });
 
     if (lazyLoad) {
