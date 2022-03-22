13dd63a534db80d92bf334620b84ea5d17db782b
Juan David Castro <me@juandc.co>
2022-03-22 10:10:05 -0500

    btn like

--- src/main.js
+++ src/main.js
@@ -35,9 +35,6 @@ function createMovies(
   movies.forEach(movie => {
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
-    movieContainer.addEventListener('click', () => {
-      location.hash = '#movie=' + movie.id;
-    });
 
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
@@ -46,18 +43,29 @@ function createMovies(
       lazyLoad ? 'data-img' : 'src',
       'https://image.tmdb.org/t/p/w300' + movie.poster_path,
     );
+    movieImg.addEventListener('click', () => {
+      location.hash = '#movie=' + movie.id;
+    });
     movieImg.addEventListener('error', () => {
       movieImg.setAttribute(
         'src',
         'https://static.platzi.com/static/images/error/img404.png',
       );
-    })
+    });
+
+    const movieBtn = document.createElement('button');
+    movieBtn.classList.add('movie-btn');
+    movieBtn.addEventListener('click', () => {
+      movieBtn.classList.toggle('movie-btn--liked');
+      // DEBERIAMOS AGREGAR LA PELICULA A LS
+    });
 
     if (lazyLoad) {
       lazyLoader.observe(movieImg);
     }
 
     movieContainer.appendChild(movieImg);
+    movieContainer.appendChild(movieBtn);
     container.appendChild(movieContainer);
   });
 }
